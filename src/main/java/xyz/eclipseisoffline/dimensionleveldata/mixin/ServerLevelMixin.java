0221ade189d43692ec5f0f6eb3694ab7fe1ecfc2
Eclipse <eclipse@eclipseisoffline.xyz>
2025-02-09 17:15:45 +0000

    Make time tick in other dimensions, version bump

--- src/main/java/xyz/eclipseisoffline/dimensionleveldata/mixin/ServerLevelMixin.java
+++ src/main/java/xyz/eclipseisoffline/dimensionleveldata/mixin/ServerLevelMixin.java
@@ -1,5 +1,7 @@
 package xyz.eclipseisoffline.dimensionleveldata.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.core.Holder;
 import net.minecraft.core.RegistryAccess;
 import net.minecraft.resources.ResourceKey;
@@ -12,16 +14,18 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.dimension.DimensionType;
 import net.minecraft.world.level.dimension.LevelStem;
 import net.minecraft.world.level.storage.DimensionDataStorage;
-import net.minecraft.world.level.storage.LevelData;
 import net.minecraft.world.level.storage.LevelStorageSource;
 import net.minecraft.world.level.storage.ServerLevelData;
 import net.minecraft.world.level.storage.WritableLevelData;
+import net.minecraft.world.level.timers.TimerQueue;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import xyz.eclipseisoffline.dimensionleveldata.DimensionLevelData;
 
@@ -38,6 +42,8 @@ public abstract class ServerLevelMixin extends Level {
     @Final
     @Mutable
     private ServerLevelData serverLevelData;
+    @Unique
+    private boolean primaryLevel = true;
 
     protected ServerLevelMixin(WritableLevelData levelData, ResourceKey<Level> dimension, RegistryAccess registryAccess, Holder<DimensionType> dimensionTypeRegistration, boolean isClientSide, boolean isDebug, long biomeZoomSeed, int maxChainedNeighborUpdates) {
         super(levelData, dimension, registryAccess, dimensionTypeRegistration, isClientSide, isDebug, biomeZoomSeed, maxChainedNeighborUpdates);
@@ -51,6 +57,26 @@ public abstract class ServerLevelMixin extends Level {
         if (serverLevelData instanceof DerivedLevelDataAccessor derived) {
             this.serverLevelData = DimensionLevelData.createForLevel(getDataStorage(), derived.getWorldData(), derived.getWrapped());
             ((LevelAccessor) this).setLevelData(this.serverLevelData);
+            primaryLevel = false;
+        }
+    }
+
+    @Redirect(method = "tickTime", at = @At(value = "FIELD", target = "Lnet/minecraft/server/level/ServerLevel;tickTime:Z"))
+    public boolean alwaysTickTime(ServerLevel instance) {
+        return true;
+    }
+
+    @WrapOperation(method = "tickTime", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/storage/ServerLevelData;setGameTime(J)V"))
+    public void noGameTimeTickWhenDerived(ServerLevelData instance, long gameTime, Operation<Void> original) {
+        if (primaryLevel) {
+            original.call(instance, gameTime);
+        }
+    }
+
+    @WrapOperation(method = "tickTime", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/timers/TimerQueue;tick(Ljava/lang/Object;J)V"))
+    public void noScheduledEventsWhenDerived(TimerQueue<MinecraftServer> instance, Object object, long gameTime, Operation<Void> original) {
+        if (primaryLevel) {
+            original.call(instance, object, gameTime);
         }
     }
 }
