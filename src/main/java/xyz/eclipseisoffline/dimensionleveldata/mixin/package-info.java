4a368fb06e76941dc3b9f3a48c49c770b89f3cbe
Eclipse <eclipse@eclipseisoffline.xyz>
2025-11-19 10:48:34 +0000

    Update to 25w46a

---
+++ src/main/java/xyz/eclipseisoffline/dimensionleveldata/mixin/package-info.java
@@ -0,0 +1,4 @@
+@NullMarked
+package xyz.eclipseisoffline.dimensionleveldata.mixin;
+
+import org.jspecify.annotations.NullMarked;
---
+++ src/main/java/xyz/eclipseisoffline/dimensionleveldata/package-info.java
@@ -0,0 +1,4 @@
+@NullMarked
+package xyz.eclipseisoffline.dimensionleveldata;
+
+import org.jspecify.annotations.NullMarked;
